985aab2df5c9d4c949c2afbebc365116c9fced5e
HieuCR7 <hieuhv97@gmail.com>
2018-08-06 14:17:38 +0700

    Add password reset(chapter12)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def edit; end
 
   def update
-    if @user.update_attributes(user_params)
+    if @user.update_attributes user_params
       flash[:success] = t ".flash_success"
       redirect_to @user
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -29,8 +29,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
-    assert_not user.activated?
-    log_in_as(user)
+    assert_not user.activated? log_in_as(user)
     assert_not is_logged_in?
     get edit_account_activation_path("invalid token", email: user.email)
     assert_not is_logged_in?
