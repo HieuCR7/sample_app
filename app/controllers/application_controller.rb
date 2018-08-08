842740fc3675024dcc04bdf29d5d200fcb85d9e5
HieuCR7 <hieuhv97@gmail.com>
2018-08-08 16:22:29 +0700

    Finish user signup(chapter7)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   before_filter :set_locale
   private
   def set_locale
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
 
   def hello
     render html: "hello, world!"
+  end
+
+  def not_found
+    render file: "#{Rails.root}/public/404.html", status: 404
   end
 end
